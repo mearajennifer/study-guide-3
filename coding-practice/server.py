71479ef37992ade8ed420579e38e2118d531f0a2
mearajennifer <jenniferlmeara@gmail.com>
2018-07-07 21:47:02 -0700

    Complete coding practice

--- coding-practice/server.py
+++ coding-practice/server.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, session, request
 app = Flask(__name__)
 app.secret_key = "blahhhhhhhh"
 
+
 @app.route('/')
 def show_homepage():
     return render_template('homepage.html')
@@ -16,11 +17,51 @@ def show_homepage():
 
 @app.route('/form')
 def show_form():
-    pass
+    """Renders the template for form.html"""
+
+    return render_template("form.html")
+
 
 @app.route('/results')
 def show_results():
-    pass
+    """Renders the template for results.html"""
+
+    if request.args.get('cheery'):
+        msg_type = "cheery"
+        message = """
+        Life’s like a movie. Write your own ending.
+        """
+        auth = "— Kermit the Frog"
+    elif request.args.get('dreary'):
+        msg_type = "dreary"
+        message = """
+        Bad days happen to everyone, but when one happens to you, just
+        keep doing your best and never let a bad day make you feel bad
+        about yourself.
+        """
+        author = "— Big Bird"
+    elif request.args.get('honest'):
+        msg_type = "honest"
+        message = """
+        Who care if me eat carrot or collard greens? Me also like broccoli
+        and lettuce and lima beans. Me still Cookie Monster. That not a sham.
+        """
+        author = "— Cookie Monster"
+
+    return render_template("results.html",
+                           message=message,
+                           author=author,
+                           msg_type=msg_type)
+
+
+@app.route('/save-name')
+def save_name():
+    """Saves person's name in a session and returns to homepage"""
+
+    name = request.args.get("person")
+    session['name'] = name
+
+    return render_template('homepage.html')
 
 
 if __name__ == "__main__":
